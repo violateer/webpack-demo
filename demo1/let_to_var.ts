5eea40d1616925de0c71e943f4305d7d485c2ab8
violateer <1828257089@qq.com>
2021-03-09 20:14:26 +0800

    let to var

--- demo1/let_to_var.ts
+++ demo1/let_to_var.ts
@@ -4,4 +4,14 @@ import generate from '@babel/generator';
 
 const code = `let a = 'let'; let b = 2`;
 const ast = parse(code, { sourceType: 'module' });
-console.log(ast);
+traverse(ast, {
+  enter: (item) => {
+    if (item.node.type === 'VariableDeclaration') {
+      if (item.node.kind === 'let') {
+        item.node.kind = 'var';
+      }
+    }
+  },
+});
+const result = generate(ast, {}, code);
+console.log(result.code);
